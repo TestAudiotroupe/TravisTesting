664ad23198118e0d2f4527b165bee88e571f8da8
Deepa Priolkar <deepa.priolkar@macys.com>
2016-03-14 00:28:43 -0700

    Testing Travis

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,10 @@ const zip = require('gulp-zip');
 gulp.task('default', function () {
 	return gulp.src('*')
 		.pipe(zip('test.zip'))
-		.pipe(gulp.dest('target'));
+		.pipe(gcPub({
+       			bucket: 'binary_artifa',
+        		keyFilename: 'key/cloud.json',
+        		projectId: 'feisty-audio-109522',
+        		public: false
+      }));
 });
